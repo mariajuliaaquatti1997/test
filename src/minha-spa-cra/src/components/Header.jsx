de9986d4b972cb2e949595dd68508c98c8f12414
MARIA JULIA AQUATTI <maria.jaquatti@senacsp.edu.br>
2025-09-27 11:54:35 -0300

    first commit

---
+++ src/minha-spa-cra/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <nav style={{ padding: '1rem', backgroundColor: '#334155', marginBottom: '1rem' }}>
+      <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
+      <Link to="/about" style={{ color: 'white' }}>Sobre</Link>
+    </nav>
+  );
+}
+export default Header;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+function HomePage(){
+return (
+    <h1>Olá mundo!!!</h1>
+)
+}
+export default HomePage
+function Texto () {
+    return (
+        <h1>Olá Senac</h1>
+
+    )
+}
